f63f66036640fd57ab942685ab9f01582611f51e
Gustavo Tironi <b51083@fgv.edu.br>
2023-11-19 16:12:19 -0300

    Alterei os prints
    Para exibir os resultados de uma maneira melhor.

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ from data_handle import global_temperature_data
 from sklearn.linear_model import Ridge
 from sklearn.preprocessing import PolynomialFeatures
 from sklearn.pipeline import make_pipeline
-from sklearn.model_selection import train_test_split
 from ridge_model import ridge_fit, ridge_predict
 
 alpha = 1
@@ -37,21 +36,41 @@ poly_predictions = poly_reg.predict(x_test) #predict data of test set with ridge
 
 alphas = [10**i for i in range(-2, 5)] #set the values of alpha to test
 poly_predictions_alphas = []
+
 for alpha in alphas: #execute the model with each alpha
-    model = make_pipeline(PolynomialFeatures(2), Ridge(alpha=alpha))
+    model = make_pipeline(PolynomialFeatures(2), Ridge(alpha=alpha)) ### Aqui precisei normalizar por que o grau estava fazendo os valores ficarem muito grandes.
     model.fit(x_train, y_train)
     poly_predictions_alphas.append(model.predict(x_test))
     
-# print([poly_predictions_alphas[i][0] for i in range(len(alphas))])
+# for i in range(len(alphas)): #print the results in the terminal
+#     print(f'Alpha {alphas[i]}: {poly_predictions_alphas[i][0]}')
 
 # Polynomial multiple degrees ------------------------------------------------------------------------------------------ 
 
-degrees = [1, 2, 4, 6]
+degrees = [1, 2, 4, 6] #set the polynom degress to test
 multi_poly_predictions = []
 
-for i in degrees:
-    model = make_pipeline(PolynomialFeatures(i), Ridge(alpha=1))
+for i in degrees: #execute the model for each degree
+    model = make_pipeline(PolynomialFeatures(i), Ridge(alpha=1)) ### Aqui precisei normalizar por que o grau estava fazendo os valores ficarem muito grandes.
     model.fit(x_train, y_train)
     multi_poly_predictions.append(model.predict(x_test))
+
+# for i in range(len(degrees)): #print the results in the terminal
+#     print(f'Degree {degrees[i]}: {multi_poly_predictions[i][0]}')
+
+# Polynomial multiple degrees alpha test ------------------------------------------------------------------------------------------
+
+alphas = [10**i for i in range(0, 5)] #set the values of alpha to test
+degrees = [1, 2, 4, 6]
+multi_poly_predictions_alphas = []
+
+for i in degrees:
+    for alpha in alphas:
+        model = make_pipeline(PolynomialFeatures(i), Ridge(alpha=alpha)) ### Aqui precisei normalizar por que o grau estava fazendo os valores ficarem muito grandes.
+        model.fit(x_train, y_train)
+        multi_poly_predictions_alphas.append(model.predict(x_test))
     
-print([multi_poly_predictions[i][0] for i in range(len(degrees))])
+# for i in range(len(degrees)): #print the results in the terminal
+#     print('')
+#     for j in range(len(alphas)):
+#         print(f'Degree {degrees[i]} and Alpha {alphas[j]}: {multi_poly_predictions_alphas[i*j][0]}')
